d51bb3ceb75e78eb8c1a1bbbbf094e78f119ecf5
Adam Jubert <jubert.adam@gmail.com>
2017-02-15 11:37:34 -0500

    Add splash directory

---
+++ frontend/components/splash/splash.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router';
+
+class Splash extends React.Component {
+
+  render() {
+    return (
+      <div className='splash-main'>
+        <h1>See what's new</h1>
+      </div>
+    );
+  }
+
+}
