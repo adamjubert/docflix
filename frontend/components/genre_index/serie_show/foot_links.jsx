b643ceb6efb7dab025cdcfa8c6a6a338bffdaab8
Adam Jubert <jubert.adam@gmail.com>
2017-02-17 20:41:06 -0500

    Add link switch functionality on show page, fixed bug

--- frontend/components/genre_index/serie_show/foot_links.jsx
+++ frontend/components/genre_index/serie_show/foot_links.jsx
@@ -2,28 +2,50 @@ import React from 'react';
 
 class FootLinks extends React.Component {
 
-  footers() {
-
-  }
-  render() {
-    const activeTab = 'selected';
-    return (
-      <div className='foot-links'>
-        <ul>
-          <li onClick={this.props.onTabChosen.bind(this, 0)}
-              className={ activeTab }
-              >Overview</li>
-          <li onClick={this.props.onTabChosen.bind(this, 1)}
-              className={ activeTab }
-              >Episodes</li>
-          <li onClick={this.props.onTabChosen.bind(this, 2)}
-              className={ activeTab }
-              >Details</li>
-        </ul>
-      </div>
-
+  footerLinks() {
+    const activeTab = 'active-foot-link';
+    const links = ['Overview', 'Episodes', 'Details'];
+    return(
+      links.map((link, index) => (
+        <li onClick={this.props.onTabChosen.bind(this, index)}
+            className={ index === this.props.selectedPane ? 'bb active-foot-link' : 'bb foot-link' }
+            >{link}</li>
+        ))
     );
+  }
+
+
+
+ render() {
+   return (
+     <div className='foot-links'>
+       <ul>
+         {this.footerLinks()}
+       </ul>
+     </div>
+
+   );
  }
 }
 
 export default FootLinks;
+
+// render() {
+//   const activeTab = 'active-foot-link';
+//   return (
+//     <div className='foot-links'>
+//       <ul>
+//         <li onClick={this.props.onTabChosen.bind(this, 0)}
+//             className={ activeTab }
+//             >Overview</li>
+//         <li onClick={this.props.onTabChosen.bind(this, 1)}
+//             className={ activeTab }
+//             >Episodes</li>
+//         <li onClick={this.props.onTabChosen.bind(this, 2)}
+//             className={ activeTab }
+//             >Details</li>
+//       </ul>
+//     </div>
+//
+//   );
+// }
