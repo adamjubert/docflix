f0ae24a04761ca4a49a873b30c6d7379fffefbf9
Adam Jubert <jubert.adam@gmail.com>
2017-02-18 12:05:56 -0500

    Footer links open in new window

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -8,9 +8,9 @@ class Footer extends React.Component {
         <div className='footer-container'>
           <h4>Questions? Call <a href='tel:3215019040' className='footer-link'>321-501-9040</a></h4>
           <ul>
-            <li><a href="http://adamjubert.com" className='footer-link'>Website</a></li>
-            <li><a href="https://github.com/adamjubert" className='footer-link'>Github</a></li>
-            <li><a href="https://linkedin.com/in/adam-jubert-1b110989" className='footer-link'>LinkedIn</a></li>
+            <li><a target="_blank" href="http://adamjubert.com" className='footer-link'>Website</a></li>
+            <li><a target="_blank" href="https://github.com/adamjubert" className='footer-link'>Github</a></li>
+            <li><a target="_blank" href="https://linkedin.com/in/adam-jubert-1b110989" className='footer-link'>LinkedIn</a></li>
           </ul>
         </div>
       </footer>
