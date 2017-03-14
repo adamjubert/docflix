51caa87597cc2d2bad59fa708b4c288e9ac137be
Adam Jubert <jubert.adam@gmail.com>
2017-03-14 00:29:57 -0400

    Change splash button

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -28,7 +28,7 @@ class Splash extends React.Component {
         <h1>Discover new stories.</h1>
         <h2><span>Watch anywhere.</span> <span>Cancel anytime.</span></h2>
         <ul className='splash-buttons'>
-          <Link to='/signup' className='splash-btn-signup'>Join free for a month</Link>
+          <Link to='/signup' className='splash-btn-signup'>Join free for a lifetime</Link>
           <Link className='splash-btn-signup' onClick={this.handleGuestLogin.bind(this)}>Sign in as guest</Link>
         </ul>
 
